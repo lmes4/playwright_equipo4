40478acbcd030c4fc48b0c611a2d0ba001ecb4ae
Jenniffer <jlrlorente4@gmail.com>
2026-05-04 17:53:16 +0200

    Corrección

--- tests/test_contacto.py
+++ tests/test_contacto.py
@@ -7,16 +7,16 @@ def test_enviar_formulario_con_campos_obligatorios_validos(page: Page):
     page.get_by_role("textbox", name="Nombre *").fill("Marta Díaz")
     print("And rellena el email con “test_automation@test.com”")
     page.get_by_role("textbox", name="Email *").fill("test_automation@test.com")
-    print("and rellena el mensaje con “test mensaje”")
+    print("And rellena el mensaje con “test mensaje”")
     page.get_by_role("textbox", name="Mensaje *").fill("test mensaje")
     print("And envía el formulario")
     expect(page.get_by_role("button", name="Enviar Mensaje")).to_be_visible()
 
 
-def test_enviar_formulario_con_campo_obligatorio_email_vacío(page: Page):
+def test_enviar_formulario_con_campo_obligatorio_email_vacio(page: Page):
     print("Given la usuaria abre la página de contacto “https://web-qa.dev.adalab.es/contact ”")
     page.goto("https://web-qa.dev.adalab.es/contact")
-    print("Cuando rellena el campo obligatorio nombre con “Marta Diaz”")
+    print("When rellena el campo obligatorio nombre con “Marta Diaz”")
     page.get_by_role("textbox", name="Nombre *").fill("Marta Díaz")
     print("And rellena el campo obligatorio mensaje con “test mensaje”")
     page.get_by_role("textbox", name="Mensaje *").fill("test mensaje")
