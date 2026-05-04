7d80bfcd468f7786fcafc18f9fb4e0e97e9b2a9a
Jenniffer <jlrlorente4@gmail.com>
2026-05-04 18:46:52 +0200

    Añadido pasos en formulario con email vacío

--- tests/test_contacto.py
+++ tests/test_contacto.py
@@ -21,6 +21,8 @@ def test_enviar_formulario_con_campo_obligatorio_email_vacio(page: Page):
     print("And rellena el campo obligatorio mensaje con “test mensaje”")
     page.get_by_role("textbox", name="Mensaje *").fill("test mensaje")
     print("And pulsa enviar")
+    page.get_by_role("button", name="Enviar Mensaje")
+    print("Then se muestra el mensaje de error el mensaje es obligatorio")
     expect(page.get_by_role("El email es obligatorio")).to_be_visible()
 
 
