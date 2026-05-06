219747c71c9b105492127b71904076c724695974
Lorena E <lorenasimarro4@gmail.com>
2026-05-06 20:36:34 +0200

    Update test_productos.py

--- tests/test_productos.py
+++ tests/test_productos.py
@@ -6,7 +6,7 @@ def test_ver_productos(page: Page):
     print("Then el usuario ve la categoría del producto “plantas”")
     expect(page.get_by_text("Plantas").nth(2)).to_be_visible()
     print("And el usuario ve el nombre del producto “Fycus Lyrata”")
-    page.get_by_role("heading", name="Ficus Lyrata").click()
+    expect(page.get_by_role("heading", name="Ficus Lyrata")).to_be_visible()
     print("And el usuario ve el precio del producto “35.00 euros”")
-    page.get_by_text("35.00 €").click()
+    expect(page.get_by_text("35.00 €")).to_be_visible()
 
