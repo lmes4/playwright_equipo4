7bf4871c0075a7c05e469aafabf737005f6b5014
Jenniffer <jlrlorente4@gmail.com>
2026-05-10 21:17:31 +0200

    POM contacto sin email

--- tests/test_POM_contacto.py
+++ tests/test_POM_contacto.py
@@ -26,23 +26,22 @@ def test_enviar_formulario_con_campos_obligatorios_validos(page: Page):
     contact_page.verify_message_form("¡Mensaje enviado con éxito!")
 
 #Realizado por Jenniffer
-def test_enviar_formulario_con_campo_obligatorio_email_vacio(page: Page):
+ddef test_form_with_required_name_field_left_empty(page: Page):
+    contact_page = ContactPage(page)
+    print("Given the users enters contact page 'Contact| Vida Verde'")
+    contact_page.open_contact_page()
 
-    print ("Given la usuaria abre la página de contacto “https://web-qa.dev.adalab.es/contact ”")
-    page.goto("https://web-qa.dev.adalab.es/contact")
-    
-    print ("When rellena el campo obligatorio nombre con “Marta Diaz”")
-    page.get_by_role("textbox", name="Nombre *").fill("ANA SANCHEZ")
+    print ("fills required email with 'test@gmail.com'")
+    contact_page.fill_contact_email("test@gmail.com")
+   
+    print ("fills required message with 'test mesage'")
+    contact_page.fill_contact_message("test mensaje")
 
-    print ("And rellena el campo obligatorio mensaje con “test mensaje”")
-    page.get_by_role("textbox", name="Mensaje *").fill("test message")
-    
-    print("And pulsa enviar")
-    page.get_by_role("button", name="Enviar Mensaje").click()
-    page.pause()
-    
-    print("Then se muestra el mensaje de error el email es obligatorio")
-    expect(page.get_by_text("El email es obligatorio")).to_be_visible()
+    print ("clicks send")
+    contact_page.press_send_contact()
+
+    print ("user should see the error message 'name is mandatory'")
+    contact_page.verify_message_form("El nombre es obligatorio")
     
 
 #Realizado por Elisabet
