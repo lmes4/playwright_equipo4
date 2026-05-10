8863a8f74d166f76a958c21577696ba53da3594f
Jenniffer <jlrlorente4@gmail.com>
2026-05-10 21:47:03 +0200

    metodo POM carrito parte 1

--- tests/test_POM_carrito.py
+++ tests/test_POM_carrito.py
@@ -1,79 +1,119 @@
 from playwright.sync_api import Page, expect
 from pages.productos_page import ProductosPage
-from pages.carrito_page import CarritoPage
+from pages.cart_page import CartPage
 
 #Realizado por Jenniffer
 def test_carrito_agregar_productos_al_carrito_ver_resumen_y_vaciar_carrito(page: Page):
+    
+    productos_page = ProductosPage(page)
+    cart_page = CartPage(page)
+    
     print("When la usuaria visita la página de productos “Nuestros Productos | Vida Verde  ”")
-    page.goto("https://web-qa.dev.adalab.es/products")
+    contact_page.open_productos_page(page)
+    
     print("And filtra por nombre “Sansevieria”")
     page.get_by_role("searchbox", name="Nombre").fill("sansevieria")
+    
     print("And añade el producto al carrito")
     page.get_by_role("button", name="Añadir Sansevieria al carrito").click()
+    
     print("And limpia el filtro")
     page.get_by_role("button", name="Quitar filtros y ver todos").click()
+    
     print("and filtra por nombre “maceta de barro”")
     page.get_by_role("searchbox", name="Nombre").fill("maceta de barro")
+    
     print("And añade el producto al carrito")
     page.get_by_role("button", name="Añadir Maceta de Barro Grande").click()
+    
     print("and visita el carrito de la compra")
     page.get_by_role("link", name="Carrito de compra").click()
+    
     print("then debe ver el nombre “Sanseveria”")
     page.get_by_role("heading", name="Sansevieria").click()
+    
     print("and su categoría “Plantas”")
     expect(page.get_by_text("Plantas")).to_be_visible()
+    
     print("and su precio “22.00 €”")
     expect(page.get_by_text("22.00 €")).to_be_visible()
+    
     print("and debe ver el producto “Maceta de Barro Grande”")
     page.get_by_role("heading", name="Maceta de Barro Grande").click()
+    
     print("and su categoría “Macetas”")
     expect(page.get_by_text("Macetas")).to_be_visible()
+    
     print("and su precio “10.50€”")
     expect(page.get_by_text("10.50 €")).to_be_visible()
+    
     print("and debe ver el resumen del pedido con los siguientes datos:")
     page.get_by_role("heading", name="Resumen del Pedido").click()
+    
     print("subtotal la suma de ambos “32.50”")
     expect(page.get_by_text("Productos (2)32.50 €")).to_be_visible()
+    
     print("con IVA 21% “6.83”")
     expect(page.get_by_text("IVA (21%)6.83 €")).to_be_visible()
+    
     print("and debe ver el total de envío “5”")
     expect(page.get_by_text("Envío5.00 €")).to_be_visible()
+    
     print("and debe ver el total “44.33”")
     expect(page.get_by_text("Total44.33 €")).to_be_visible()
+    
     print("when hace clic en vaciar carrito")
     page.get_by_role("button", name="Vaciar Carrito").click()
+    
     print("then debe ver el mensaje “tu carrito está vacío”")
     expect(page.get_by_text("Tu carrito está vacío")).to_be_visible()
 
 
 #Realizado por Jenniffer
 def test_carrito_quitar_productos_del_carrito_y_ver_resumen(page: Page):
+    
+    productos_page = ProductosPage(page)
+    cart_page = CartPage(page)
+    
     print("When la usuaria visita la página de productos “Nuestros Productos | Vida Verde  ”")
-    page.goto("https://web-qa.dev.adalab.es/products")
+    contact_page.open_productos_page(page)
+    
     print("And filtra por nombre ficus")
     page.get_by_role("searchbox", name="Nombre").fill("ficus")
+    
     print("And añade el producto al carrito")
     page.get_by_role("button", name="Añadir Ficus Lyrata al carrito").click()
+    
     print("And limpia el filtro")
     page.get_by_role("button", name="Quitar filtros y ver todos").click()
+    
     print("and filtra por nombre “tijeras”")
     page.get_by_role("searchbox", name="Nombre").fill("tijeras")
+    
     print("And añade el producto al carrito")
     page.get_by_role("button", name="Añadir Tijeras de Podar al carrito").click()
+    
     print("and visita el carrito de la compra")
     page.get_by_role("link", name="Carrito de compra").click()
+    
     print("when elimina el producto “ficus” desde la página del carrito")
     page.get_by_role("button", name="Eliminar Ficus Lyrata del carrito").click()
+    
     print("then no debe ver el producto “ficus”")
     expect(page.get_by_text("Tijeras de Podar")).to_be_visible()
+    
     print("then debe ver el resumen actualizado")
     page.get_by_role("complementary", name="Resumen del Pedido").click()
+   
     print("subtotal la suma de ambos “18.50”")
     expect(page.get_by_text("Productos (1)18.50 €")).to_be_visible()
+    
     print("and el iva “3.88”")
     expect(page.get_by_text("IVA (21%)3.88 €")).to_be_visible()
+    
     print("and debe ver el total de envío “5”")
     expect(page.get_by_text("Envío5.00 €")).to_be_visible()
+    
     print("and el total “27.38”")
     expect(page.get_by_text("Total27.38 €")).to_be_visible()
     
