0a60f5ef6e98c00aacd93179ff26ffd1e7d621d4
Lorena E <lorenasimarro4@gmail.com>
2026-05-11 16:05:59 +0200

    inicio pages compra

---
+++ pages/compra_page.py
@@ -0,0 +1,8 @@
+from playwright.sync_api import Page, expect
+
+class CompraPage:
+
+def __init__(self, page: Page):
+        self.page = page
+        self.url = 'https://web-qa.dev.adalab.es/products'
+        self.title = "Compra"
