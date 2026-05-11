e8d9b4500625700f3c1baf8e546b9c275e840d0a
Lorena E <lorenasimarro4@gmail.com>
2026-05-11 17:46:36 +0200

    correcciones y avances prodcutos

--- pages/productos_page.py
+++ pages/productos_page.py
@@ -13,7 +13,7 @@ class ProductosPage:
     def verificar_productos_category(self, category):
         expect(self.page.get_by_text(category).nth(2)).to_be_visible()
 
-    def verificar_productos_name(self,product_name):
+    def verificar_productos_name(self, product_name):
         expect(self.page.get_by_role("heading", name=product_name)).to_be_visible()
 
     def verificar_productos_price(self,price):
--- tests/test_POM_productos.py
+++ tests/test_POM_productos.py
@@ -7,13 +7,13 @@ def test_productos_page(page: Page):
     productos_page = ProductosPage(page);
 
     print("When el usuario abre la pagina de productos")
-    productos_page.open_productos_page()
+    productos_page.abrir_productos_page()
 
     print("Then el usuario ve la categoria del producto “plantas”")
-    productos_page.verify_productos_category("Plantas")
+    productos_page.verificar_productos_category("Plantas")
 
     print("And el usuario ve el nombre del producto “Fycus Lyrata”")
-    productos_page.verify_productos_name("Ficus Lyrata")
+    productos_page.verificar_productos_name("Ficus Lyrata")
 
     print("And el usuario ve el precio del producto “35.00 euros”")
-    productos_page.verify_productos_price("35.00 €")
+    productos_page.verificar_productos_price("35.00 €")
