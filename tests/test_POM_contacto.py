74fa6dd18808e6c7a6490702631cc36a81d2e779
Lorena E <lorenasimarro4@gmail.com>
2026-05-13 11:21:15 +0200

    Rehacer contactos pom jenni

--- tests/test_POM_contacto.py
+++ tests/test_POM_contacto.py
@@ -7,41 +7,40 @@ def test_enviar_formulario_con_campos_obligatorios_validos(page: Page):
     contact_page = ContactPage(page)
 
     print("Given la usuaria abre la página de contacto 'Contáctanos | Vida Verde'")
-    contact_page.open_contact_page()
+    contact_page.abrir_pagina_contactos()
 
     print("When rellena el nombre")
-    contact_page.fill_contact_name("Marta Diaz")
+    contact_page.rellenar_nombre_contacto("Marta Diaz")
     
-
     print("And rellena el email")
-    contact_page.fill_contact_email("test@gmail.com")
+    contact_page.rellena_email_contacto("test@gmail.com")
 
     print("And rellena el mensaje")
-    contact_page.fill_contact_message("test mensaje")
+    contact_page.rellena_mensaje_contacto("test mensaje")
 
     print("And pulsa el boton enviar")
-    contact_page.press_send_contact()
+    contact_page.hace_click_enviar()
 
     print("Then debería ver un mensaje de éxito")
-    contact_page.verify_message_form("¡Mensaje enviado con éxito!")
+    contact_page.verifica_mensaje("¡Mensaje enviado con exito!")
 
 #Realizado por Jenniffer
 def test_form_with_required_name_field_left_empty(page: Page):
     contact_page = ContactPage(page)
     print("Given the users enters contact page 'Contact| Vida Verde'")
-    contact_page.open_contact_page()
+    contact_page.abrir_pagina_contactos()
 
     print ("fills required email with 'test@gmail.com'")
-    contact_page.fill_contact_email("test@gmail.com")
+    contact_page.rellena_email_contacto("test@gmail.com")
    
     print ("fills required message with 'test mesage'")
-    contact_page.fill_contact_message("test mensaje")
+    contact_page.rellena_mensaje_contacto("test mensaje")
 
     print ("clicks send")
-    contact_page.press_send_contact()
+    contact_page.hace_click_enviar()
 
     print ("user should see the error message 'name is mandatory'")
-    contact_page.verify_message_form("El nombre es obligatorio")
+    contact_page.verifica_mensaje("El nombre es obligatorio")
     
 
 #Realizado por Elisabet
