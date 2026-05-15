ed8d12bd4bb299e1d27748c14ed3a6191d03a983
Lorena E <lorenasimarro4@gmail.com>
2026-05-15 19:16:48 +0200

    correcciones visionado reyes 2

--- tests/test_POM_productos.py
+++ tests/test_POM_productos.py
@@ -9,11 +9,11 @@ def test_productos_page(page: Page):
     print("When el usuario abre la página de productos")
     productos_page.abrir_productos_page()
 
-    print("Then el usuario ve la categoría del producto “plantas”")
+    print("Then el usuario ve la categoría del producto “Plantas”")
     productos_page.verificar_productos_category("Plantas")
 
     print("And el usuario ve el nombre del producto “Fycus Lyrata”")
     productos_page.verificar_productos_name("Ficus Lyrata")
 
-    print("And el usuario ve el precio del producto “35.00 euros”")
+    print("And el usuario ve el precio del producto “35.00 €")
     productos_page.verificar_productos_price("35.00 €")
