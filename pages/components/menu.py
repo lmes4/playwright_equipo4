3fc6ac988de8b0fc5b775c99af6f6f767e3e664d
Elisabet Gacía <elisabet.gar@gmail.com>
2026-05-16 12:49:58 +0100

    Change menu.py adapted mobile

--- pages/components/menu.py
+++ pages/components/menu.py
@@ -7,12 +7,51 @@ class Menu:
         self.menu_quienes_somos = "Quiénes Somos"
         self.menu_productos = "Productos"
         self.menu_contactos = "Contacto"
+        self.menu_mobile = "Abrir menú principal"
+
+    def esMobile (self):
+        ancho = self.page.viewport_size['width']
+        if ancho < 1024:
+            return True;
+        else:
+            return False;
+
 
     def abrir_menu_quienes_somos (self):
-        self.page.get_by_role("link", name=self.menu_quienes_somos).click()
+        if(self.esMobile()):
+            #Mobile
+            print("Navegando en móvil")
+            self.page.get_by_role("button", name=self.menu_mobile).click()
+            self.page.get_by_role("menuitem", name=self.menu_quienes_somos).click()
+        else:
+            #Desktop
+            print("Navegando en escritorio")
+            self.page.get_by_role("link", name=self.menu_quienes_somos).click()
+
+
+
 
     def abrir_menu_productos (self):
-        self.page.get_by_role("link", name=self.menu_productos).click()
+        if(self.esMobile()):
+            #Mobile
+            print("Navegando en móvil")
+            self.page.get_by_role("button", name=self.menu_mobile).click()
+            self.page.get_by_role("menuitem", name=self.menu_productos).click()
+        else:
+            #Desktop
+            print("Navegando en escritorio")
+            self.page.get_by_role("link", name=self.menu_productos).click()
+
+
     
     def abrir_menu_contactos (self):
-        self.page.get_by_role("link", name=self.menu_contactos).click()
+        if(self.esMobile()):
+            #Mobile
+            print("Navegando en móvil")
+            self.page.get_by_role("button", name=self.menu_mobile).click()
+            self.page.get_by_role("menuitem", name=self.menu_contactos).click()
+        else:
+            #Desktop
+            print("Navegando en escritorio")
+            self.page.get_by_role("link", name=self.menu_contactos).click()
+
--- tests/test_menu.py
+++ tests/test_menu.py
@@ -18,36 +18,33 @@ def test_visitar_los_enlaces_del_menú(page: Page):
 
     print("Cuando el usuario abre la página Inicio | Vida Verde “https://web-qa.dev.adalab.es”")
     home_page.abrir_pagina_inicio()
-
     print ("Ve el título 'Vida Verde'")
     home_page.verificar_pagina_inicio()
 
 
     print ("Hace click en 'Quiénes Somos'")
     menu.abrir_menu_quienes_somos()
-
     print ("Ve el título 'Quiénes Somos'")
     about_us_page.verificar_titulo_quienes_somos()
-
     print ("Ve la URL https://web-qa.dev.adalab.es/about")
     about_us_page.verificar_quienes_somos_url()
 
 
     print ("Hace click en 'Productos'")
     menu.abrir_menu_productos()
-
     print ("Ve el título 'Catálogo de Productos'")
     productos_page.verificar_productos_title()
-
     print ("Ve la URL https://web-qa.dev.adalab.es/products")
     productos_page.verificar_productos_url()
 
 
     print ("Hace click en 'Contacto'")
     menu.abrir_menu_contactos()
-
     print ("Ve el título 'Contáctanos'")
     contact_page.verificar_titulo_contacto()
-
     print ("Ve la URL https://web-qa.dev.adalab.es/contact")
     contact_page.verificar_contacto_url()
+
+
+
+
